18c473711a577a2e7acb26e61c40b34f1388a62d
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-09-16 13:03:49 -0600

    Task 1

--- index.js
+++ index.js
@@ -39,9 +39,37 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function (){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person('Mary', 50);
+
+personOne.eat('Banana');
+personOne.eat('Peanut Butter');
+personOne.eat('Goldfish');
+personOne.eat('String Cheese');
+personOne.eat('Cake');
+
+console.log(personOne.stomach);
+
+personOne.poop();
 
 /*
   TASK 2
