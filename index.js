cf29a3099adfeaaf6ccb8d5102825c255015983f
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-09-16 13:35:32 -0600

    Task 2

--- index.js
+++ index.js
@@ -85,8 +85,19 @@ personOne.poop();
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(miles){
+  this.odometer = this.odometer + miles;
 }
 
 /*
@@ -96,8 +107,8 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  
 }
 
 /* 
